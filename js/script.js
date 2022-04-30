7cce732c2b7845bf016f2aa451522ad0217769bc
Hovkic <hovkic15@gmail.com>
2022-04-30 14:29:11 -0700

    create dimensions button and prompt function

--- js/script.js
+++ js/script.js
@@ -4,6 +4,31 @@ container.setAttribute(
   'display: grid; grid-template-columns: repeat(16,1fr)'
 );
 
+// change dimensions button
+const outer = document.getElementById('outer-container');
+const button = document.createElement('button');
+button.textContent = 'Change Dimensions';
+outer.appendChild(button);
+
+button.addEventListener('click', doAsk);
+
+// prompt to change grid dimensions
+function doAsk() {
+  let popUp = prompt('Enter desired grid dimensions');
+
+  if (popUp < 1) {
+    alert("that won't work, too small"); // that won't work, too small
+  } else if (popUp >= 1 && popUp <= 100) {
+    columnCount = popUp;
+    makeRow(popUp * popUp); // enter working function
+    changeColor();
+  } else if (popUp > 100) {
+    alert('too high'); // that won't work, too high
+  } else {
+    alert('invalid bro'); // invalid; enter a number 1-100
+  }
+}
+
 // set row number
 function makeRow(number) {
   for (i = 0; i < number; i++) {
