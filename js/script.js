787092b96c898ef4ff6e58a63fe8b3108e49702b
Hovkic <hovkic15@gmail.com>
2022-04-30 18:58:20 -0700

    add dimensions display

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const container = document.getElementById('container');
 const btnBox = document.getElementById('btn-container');
 const button = document.createElement('button');
 button.textContent = 'Change Grid Dimensions';
+button.className = 'btn';
 btnBox.appendChild(button);
 
 button.addEventListener('click', doAsk);
@@ -13,6 +14,7 @@ function doAsk() {
   let count = prompt(
     'Enter your desired number of squares on each side. \n Min: 1 \n Max: 100.'
   );
+  const dimensions = document.querySelector('.dimensions');
 
   if (count == '' || count == null) {
     return;
@@ -21,6 +23,7 @@ function doAsk() {
   } else if (count >= 1 && count <= 100) {
     deleteRows();
     makeRow(count * count);
+    dimensions.innerText = `${count} x ${count}`;
     container.style = `grid-template-columns: repeat(${count}, 1fr)`;
     changeColor();
   } else if (count > 100) {
@@ -58,3 +61,5 @@ function changeColor() {
 }
 
 changeColor();
+
+// create display for the dimensions
