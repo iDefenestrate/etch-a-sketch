bb415d93a87dc3d2c8c6dec6bdef773e3dceb76f
Hovkic <hovkic15@gmail.com>
2022-04-30 18:24:16 -0700

    clean up prompt reponses

--- js/script.js
+++ js/script.js
@@ -1,30 +1,32 @@
 const container = document.getElementById('container');
 
 // change dimensions button
-const outer = document.getElementById('outer-container');
+const btnBox = document.getElementById('btn-container');
 const button = document.createElement('button');
-button.textContent = 'Change Dimensions';
-outer.appendChild(button);
+button.textContent = 'Change Grid Dimensions';
+btnBox.appendChild(button);
 
 button.addEventListener('click', doAsk);
 
 // prompt to change grid dimensions
 function doAsk() {
-  let count = prompt('Enter desired grid dimensions');
+  let count = prompt(
+    'Enter your desired number of squares on each side. \n Min: 1 \n Max: 100.'
+  );
 
   if (count == '' || count == null) {
     return;
   } else if (count < 1) {
-    alert("that won't work, too small"); // that won't work, too small
+    alert("That's too small! Please enter a number between 1 and 100.");
   } else if (count >= 1 && count <= 100) {
     deleteRows();
     makeRow(count * count);
     container.style = `grid-template-columns: repeat(${count}, 1fr)`;
     changeColor();
   } else if (count > 100) {
-    alert('too high'); // that won't work, too high
+    alert("That's too large! Please enter a number between 1 and 100.");
   } else {
-    alert('invalid bro'); // invalid; enter a number 1-100
+    alert('Invalid.\n Please enter a number between 1 and 100.'); // invalid; enter a number 1-100
   }
 }
 
